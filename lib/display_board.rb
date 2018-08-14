4afcfd63d4a7df47a775db1279319b5d11d85d8e
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-14 14:44:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,18 @@
 def display_board
+<<<<<<< HEAD
 puts "   |   |   "   
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
+=======
+  print "
+   |   |   
+-----------
+   |   |   
+-----------
+   |   |   "
+>>>>>>> ef1f29614ee4219e6899ec25a2c74f6556c975cd
 
 end
     
